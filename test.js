851f8eb44b40b4f8eb915d4f50d643f3677b8825
chinanf-boy <865501259@qq.com>
2018-08-25 10:57:33 +0800

    Add human-size

--- test.js
+++ test.js
@@ -14,3 +14,10 @@ test('tap', async t => {
 	t.true(!!res.pubSize);
 	t.true(!!res.insSize);
 });
+
+test('two-log@0.2.1', async t => {
+	let res = await m('two-log@0.2.1');
+	t.true(!res.version);
+	t.true(!!res.pubSize);
+	t.true(!!res.insSize);
+});
